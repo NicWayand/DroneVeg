3d4a751632ceb97bac62dc0968fcc855214f8874
NicWayand <nicway@gmail.com>
2017-09-14 15:25:30 -0600

    Remove comment

--- Classify_Vegetation_from_Drone_Imagery.py
+++ Classify_Vegetation_from_Drone_Imagery.py
@@ -27,9 +27,9 @@ VEG_Tif         = os.path.join(data_dir,'VegHeight.tif')
 VEG_mask_Tif    = os.path.join(data_dir,'VegMask.tif')
 LAI_Tif         = os.path.join(data_dir,'LAI.tif')
 
-# Load stuff in
+# Load stuff in (Chunk using dask, to avoid OOM)
 ds_dsm  = xr.open_rasterio(DSM_Tif, chunks={'x':1000, 'y':1000}).sel(band=1).drop('band')
-ds_dsm  = ds_dsm.where(ds_dsm>0).astype(np.float32).load() # da_VARI = xr.open_dataarray(r'F:\Work\e\Data\LIDAR\UAV\Fortress-20170718\VARI.nc', chunks=10)
+ds_dsm  = ds_dsm.where(ds_dsm>0).astype(np.float32).load()
 ds_rgb  = xr.open_rasterio(rgb_Tif, chunks={'x':1000, 'y':1000}) # drop band 4 which is empty
 ds_rgb = ds_rgb.where(ds_rgb.sel(band=1)>0).astype(np.float32)
 print("loaded")
@@ -52,8 +52,10 @@ ds_LAI = ds_RG.where(veg)*max_LAI
 print("Estimated LAI from GRVI")
 
 # Minimum Filter approach
-window_meters = 11 # m # should be odd, so center of filter is one pixel
-pixel_meters = 0.05 # m/pixel
+window_meters = 11 # m # should be odd, so center of filter is one pixel. Size depends on vegetatioin in domain.
+# Should be larger than max clusters of forest (you want the filter size to always see the ground around the
+# Vegetation.
+pixel_meters = 0.05 # m/pixel (Resoution of input DSM)
 window_pixels = window_meters / pixel_meters
 dsm_min = scipy.ndimage.minimum_filter(ds_dsm.values, size=window_pixels, mode='reflect')
 
